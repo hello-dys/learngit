11c9b02ea2c315b313e2f8bfe1e5586795de2ccb
dys <826482017@qq.com>
2023-03-13 23:55:57 +0800

    P3

--- P3/Graph.java
+++ P3/Graph.java
@@ -1,8 +1,6 @@
 package P3;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Graph<T> {
     //顶点的数量
@@ -13,12 +11,10 @@ public class Graph<T> {
     private ArrayList<T> vertex;
     //使用二维数组表示邻接矩阵
     private int[][] matrix;
-    //辅组数组，用来标记顶点是否被遍历,下标代表顶点
-    boolean[] mark;
 
 
     //初始化图,n指的是顶点的数量
-    Graph(int n){
+    public Graph(int n){
         //顶点集合的大小是顶点的个数
         vertex = new ArrayList<>(n);
         //顶点的个数是二维数组的长度
@@ -26,7 +22,17 @@ public class Graph<T> {
         //顶点数量、边数量初始化为0
         vCount = 0;
         edgeCount = 0;
-        mark = new boolean[n];
+    }
+
+    static ArrayList<String> s = new ArrayList<String>();
+    static class Person {
+        int i = 0;
+        String name = null;
+        public Person(String n) {
+            s.add(n);
+            i = s.size() - 1;
+            name = n;
+        }
     }
 
     //插入节点
@@ -36,12 +42,13 @@ public class Graph<T> {
     }
 
     //添加边
-    public void addEdge(int v1, int v2, int weight){
-        matrix[v1][v2] = weight;
-        matrix[v2][v1] = weight;
+    public void addEdge(Person p1, Person p2){
+        int v1 = p1.i;
+        int v2 = p2.i;
+        matrix[v1][v2] = 1;
+        matrix[v2][v1] = 1;
         edgeCount++;
     }
-
     //获取图中顶点的个数
     public int getvCount(){
         return vCount;
@@ -52,108 +59,27 @@ public class Graph<T> {
         return edgeCount;
     }
 
-    //显示邻接矩阵表示的图
-    public void print(){
-        for (int[] o : matrix) {
-            for (int i : o) {
-                System.out.print(i+"  ");
-            }
-            System.out.println();
-        }
-    }
-
-    //得到第一个邻接节点的下标
-    public int getFirst(int v){
-        for (int i = 0; i < vertex.size(); i++) {
-            if(matrix[v][i] == 1){
-                return i;
+    public int getDistance(Person p1, Person p2) {
+        int[][] graph = matrix;
+        for (int i = 0; i < getvCount(); i++){
+            for (int j = 0; j < getvCount(); j++){
+                if (graph[i][j] == 0)
+                    graph[i][j] = 9999;
             }
         }
-        return -1;
-    }
-
-    //根据前一个邻接节点的下标获取下一个邻接节点
-    public int getNext(int v1,int v2){
-        for(int i = v2+1; i < vertex.size(); i++){
-            if(matrix[v1][i] == 1){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-
-    public void dfs(){
-        //遍历所有的节点，进行dfs回溯
-        for (int i = 0; i < getvCount(); i++) {
-            //判断顶点是否被搜索
-            if(!mark[i]){
-                dfs2(i);
-            }
-        }
-        //复原标记辅组数组的状态
-        for (int i = 0; i < mark.length; i++) {
-            mark[i] = false;
-        }
-    }
-
-    public void dfs( int v){
-        System.out.print(vertex.get(v)+"——>");
-        //将vertex标记为已经搜索
-        mark[v] = true;
-        int w = getFirst(v);
-        while (w != -1){
-            if(!mark[w]){
-                dfs(w);
-            }
-            w = getNext(v,w);
-            //相通的顶点+1
-        }
-    }
-
-
-    public void dfs2(int v){
-        mark[v] = true;
-        System.out.print(vertex.get(v)+"——>");
-        for (int i = 0; i < vertex.size(); i++) {
-            if(matrix[v][i] == 1 && !mark[i]){
-                dfs(i);
-            }
-        }
-    }
-
-
-    public void bfs(){
-        for (int i = 0; i < vertex.size(); i++) {
-            if(!mark[i]){
-                bfs(i);
-            }
-        }
-
-        for (int i = 0; i < mark.length; i++) {
-            mark[i] = false;
-        }
-    }
-
-    //深度优先遍历
-    public void bfs(int v){
-        Queue queue = new LinkedList();
-        int next;
-        mark[v] = true;
-        queue.add(v);
-        while (!queue.isEmpty()){
-            next = (int) queue.remove();
-            System.out.print(vertex.get(next)+"——>");
-            int vex = getFirst(next);
-            while (vex != -1){
-                if(mark[vex] == false){
-                    queue.add(vex);
-                    mark[vex] = true;
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph.length; j++) {
+                for (int k = 0; k < graph.length; k++) {
+                    if (graph[j][i] + graph[i][k] < graph[j][k]) {
+                        graph[j][k] = graph[j][i] + graph[i][k];
+                    }
                 }
-                vex = getNext(next,vex);
             }
         }
+        if (graph[p1.i][p2.i] == 9999){
+            return -1;
+        }
+        return graph[p1.i][p2.i];
     }
-
 }
 
--- P3/GraphTest.java
+++ P3/GraphTest.java
@@ -3,42 +3,42 @@ package P3;
 public class GraphTest {
 
     public static void main(String[] args) {
+
+        Graph.Person aa = new Graph.Person("aaa");
+        Graph.Person bb = new Graph.Person("bbb");
+        Graph.Person cc = new Graph.Person("ccc");
+        Graph.Person dd = new Graph.Person("ddd");
+        Graph.Person ee = new Graph.Person("eee");
+        Graph.Person ff = new Graph.Person("fff");
+        Graph.Person gg = new Graph.Person("ggg");
+
         //存放顶点的数组
-        String vertex[] = {"A", "B", "C", "D", "E", "F", "G"};
+        String vertex[] = Graph.s.toArray(new String[0]);
 
         //将数组的长度传入无向图对象
         Graph<String> graph = new Graph<>(vertex.length);
 
         //将顶点添加到图的顶点集合中
-        for (String s : vertex) {
-            graph.addVertex(s);
-        }
+        graph.addVertex(aa.name);
+        graph.addVertex(bb.name);
+        graph.addVertex(cc.name);
+        graph.addVertex(dd.name);
+        graph.addVertex(ee.name);
+        graph.addVertex(ff.name);
+        graph.addVertex(gg.name);
 
         //添加边
-        graph.addEdge(0, 1, 1);
-        graph.addEdge(0, 2, 1);
-        graph.addEdge(0, 5, 1);
-        graph.addEdge(1, 2, 1);
-        graph.addEdge(1, 3, 1);
-        graph.addEdge(1, 4, 1);
-        graph.addEdge(5, 6, 1);
+        graph.addEdge(aa, bb);
+        graph.addEdge(aa, cc);
+        graph.addEdge(aa, ff);
+        graph.addEdge(bb, cc);
+        graph.addEdge(bb, dd);
+        graph.addEdge(bb, ee);
+        graph.addEdge(ff, gg);
 
-
-        System.out.println("----------测试无向图中顶点的数量:-----------------");
-        System.out.println(graph.getvCount());
-
-        System.out.println("----------测试无向图中边的数量:-----------------");
-        System.out.println(graph.getEdgeCount());
-
-        System.out.println("----------邻接矩阵表示无向图测试:-----------------");
-        graph.print();
-
-        System.out.println("----------测试深度优先遍历:----------------------");
-        graph.dfs();
-
-        System.out.println();
-        System.out.println("----------广度遍历优先测试:--------------------------");
-        graph.bfs();
+        //两点之间的距离
+        System.out.println(graph.getDistance(aa, gg));
+        System.out.println(graph.getDistance(bb, gg));
     }
 
 }
